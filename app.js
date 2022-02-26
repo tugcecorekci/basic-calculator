a0e6b086a9aa58c0e67d8a844e0eb0f388c8a6c1
tugcecorekci <tugcecorekcioglu@gmail.com>
2022-02-27 00:31:33 +0100

    Added functions to action-buttons.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let screen = document.querySelector('.operationScreen')
 screen.textContent = "0"
 let firstValue = 0
-let operation = function () { }
+let operation = null
 
 function getValue() {
     for (let i = 0; i < 10; i++) {
@@ -15,12 +15,27 @@ function clickNum(a) {
     if (screen.textContent == "0") {
         screen.textContent = button.value
     }
+    else if (screen.textContent == "-0") {
+        screen.textContent = button.value * (-1)
+    }
     else {
         screen.textContent += button.value
     }
 }
 
 getValue()
+
+const plusBtn = document.querySelector('#plus')
+const minusBtn = document.querySelector('#minus')
+const multiBtn = document.querySelector('#multi')
+const divideBtn = document.querySelector('#divide')
+const equalBtn = document.querySelector('#equal')
+
+plusBtn.addEventListener('click', plusFunc)
+minusBtn.addEventListener('click', minusFunc)
+multiBtn.addEventListener('click', multiFunc)
+divideBtn.addEventListener('click', divideFunc)
+equalBtn.addEventListener('click', () => operation())
 
 function add() {
     let a = parseInt(screen.textContent)
@@ -46,32 +61,23 @@ function divide() {
     screen.textContent = result
 }
 
-const plusBtn = document.querySelector('#plus')
-plusBtn.addEventListener('click', plusFunc)
-
 function plusFunc() {
     firstValue = parseInt(screen.textContent)
     operation = add
     screen.textContent = "0"
 }
-const minusBtn = document.querySelector('#minus')
-minusBtn.addEventListener('click', minusFunc)
 
 function minusFunc() {
     firstValue = parseInt(screen.textContent)
     operation = minus
     screen.textContent = "0"
 }
-const multiBtn = document.querySelector('#multi')
-multiBtn.addEventListener('click', multiFunc)
 
 function multiFunc() {
     firstValue = parseInt(screen.textContent)
     operation = multi
     screen.textContent = "0"
 }
-const divideBtn = document.querySelector('#divide')
-divideBtn.addEventListener('click', divideFunc)
 
 function divideFunc() {
     firstValue = parseInt(screen.textContent)
@@ -79,5 +85,35 @@ function divideFunc() {
     screen.textContent = "0"
 }
 
-const equalBtn = document.querySelector('#equal')
-equalBtn.addEventListener('click', () => operation())
+const clearBtn = document.querySelector('#clear')
+clearBtn.addEventListener('click', clearFunc)
+
+function clearFunc() {
+    screen.textContent = "0"
+    firstValue = 0
+    operation = null
+}
+
+const percentBtn = document.querySelector('#percent')
+percentBtn.addEventListener('click', percentFunc)
+
+function percentFunc() {
+    firstValue = parseInt(screen.textContent)
+    if (firstValue == 0) return
+    let result = firstValue / 100
+    screen.textContent = result
+}
+
+const pmBtn = document.querySelector('#pm')
+pmBtn.addEventListener('click', pmFunc)
+
+function pmFunc() {
+    if (screen.textContent === "0") {
+        screen.textContent = "-0"
+    }
+    else {
+        firstValue = parseInt(screen.textContent)
+        firstValue = -firstValue
+        screen.textContent = firstValue
+    }
+} 
